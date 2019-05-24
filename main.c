3593bc2d9eb0620026d027417df1b8ba63e2a6a1
Adriano Gomes <adrianogsss@gmail.com>
2019-05-24 20:44:11 -0300

    Adição de função de menu e inicialização do jogo

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define TAMANHO_TABULEIRO 8
+
 
 int inicializaTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]){
     int i,j;
@@ -22,9 +24,49 @@ int printaTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]){
     printf("  a b c d e f g h\n");
 }
 
+void inicializaJogo(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]){
+    inicializaTabuleiro(tabuleiro);
+    // @TODO: Inicializar variáveis de jogo
+}
+
+
+int mostraMenu(){
+
+    char resposta[1];
+    printf("\n Seja bem vindo ao jogo de Damas Anglo-Americanas");
+    printf("\n Deseja jogar como player 1 ou 2? (n para sair)");
+    scanf("%c", resposta);
+
+    if(!strcmp(resposta,"1")){
+        return 1;
+    }else if(!strcmp(resposta,"2")){
+        return 2;
+    }else{
+        return 0;
+    }
+
+}
+
+
+
 int main(){
     int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
-    
-    inicializaTabuleiro(tabuleiro);
-    printaTabuleiro(tabuleiro);
+    int jogar = 1;
+    int menu = 0;
+
+    jogar = mostraMenu();
+    if(jogar){
+        inicializaJogo(tabuleiro);
+        
+    }
+    while(jogar){
+        
+        printaTabuleiro(tabuleiro);
+        if(menu){
+            jogar = mostraMenu();
+            if(jogar){
+                inicializaJogo(tabuleiro);
+            }
+        }
+    }
 }
