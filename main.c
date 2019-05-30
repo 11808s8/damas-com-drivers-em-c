75cf6073a507b396ce9106b11fb1f00f46954ab9
Bruno Caregnato <33569494+brunocaregnato@users.noreply.github.com>
2019-05-29 21:49:31 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -12,55 +12,86 @@ int inicializaTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]){
             tabuleiro[i][j] = 0;
 }
 
-int printaTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]){
-    int i,j;
-    
-    for(i=0;i<TAMANHO_TABULEIRO;i++){
-        printf("%d", i);
-        for(j=0;j<TAMANHO_TABULEIRO;j++)
-            printf(" %d", tabuleiro[i][j]);
-        printf("\n");
+char alimentatabuleiro(int posicao){
+
+    switch(posicao)
+    {
+  	case 0:
+            return ' ';
+        case 1:
+            return ' '; //espaço vazio
+        case 2:
+            return 'b'; //peça normal branca
+        case 3:
+            return 'v'; //peça normal vermelha
+        case 4:
+            return 'B'; //dama branca
+        case 5:
+            return 'V'; //dama vermelha
     }
-    printf("  a b c d e f g h\n");
+    return ('?');
+}
+
+int printaTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]){
+    int linhas, colunas;
+    printf("\n");
+    printf("    a   b   c   d   e   f   g   h\n"); //Colunas
+    printf("  +---+---+---+---+---+---+---+---+\n");
+
+    for (linhas=0; linhas<TAMANHO_TABULEIRO; ++linhas)
+    {
+        printf("%d |", linhas+1); //Linhas
+
+        for (colunas=0; colunas<TAMANHO_TABULEIRO; ++colunas) //Tamanho do tabuleiro
+        {
+            printf(" %c |", alimentatabuleiro(tabuleiro[linhas][colunas]));
+        }
+        printf("\n");
+        printf("  +---+---+---+---+---+---+---+---+\n");
+    }
 }
 
 void inicializaJogo(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]){
-    inicializaTabuleiro(tabuleiro);
+    //inicializaTabuleiro(tabuleiro);
     // @TODO: Inicializar variáveis de jogo
 }
 
 
 int mostraMenu(){
 
-    char resposta[1];
+    int resposta;
     printf("\n Seja bem vindo ao jogo de Damas Anglo-Americanas");
     printf("\n Deseja jogar como player 1 ou 2? (n para sair)");
-    scanf("%c", resposta);
+    scanf("%d", &resposta);
 
-    if(!strcmp(resposta,"1")){
-        return 1;
-    }else if(!strcmp(resposta,"2")){
-        return 2;
-    }else{
-        return 0;
-    }
+    if(resposta==1) return 1;
+    else if(resposta==2) return 2;
+
+    return 0;
 
 }
 
-
-
 int main(){
-    int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
     int jogar = 1;
     int menu = 0;
+    int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO] = {
+    {0,2,0,2,0,2,0,2},
+    {2,0,2,0,2,0,2,0},
+    {0,2,0,2,0,2,0,2},
+    {1,0,1,0,1,0,1,0},
+    {0,1,0,1,0,1,0,1},
+    {3,0,3,0,3,0,3,0},
+    {0,3,0,3,0,3,0,3},
+	{3,0,3,0,3,0,3,0}};
+
 
     jogar = mostraMenu();
     if(jogar){
         inicializaJogo(tabuleiro);
-        
+
     }
     while(jogar){
-        
+
         printaTabuleiro(tabuleiro);
         if(menu){
             jogar = mostraMenu();
@@ -68,5 +99,6 @@ int main(){
                 inicializaJogo(tabuleiro);
             }
         }
+        break;
     }
 }
